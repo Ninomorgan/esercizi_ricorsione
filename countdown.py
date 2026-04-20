3d254931f2489bd26211d15bb944f056f4688927
Ninomorgan <s325433@studenti.polito.it>
2026-04-20 19:04:09 +0200

    introduzione esercizi ricorsione countdown,binomial,palyndormee

--- countdown.py
+++ countdown.py
@@ -1,21 +1,29 @@
 from time import sleep
 
+
+
+
+#metodo normale
 def countdown(n):
-    while n >= 0:
+    while n>=0:
         print(n)
         sleep(1)
-        n -= 1
+        n-=1
+    pass
 
-def countdown_recursive(n):
+#metodo ricorsione
+
+def countdown_ricorsione(n):
     #condizione terminale
-    if n == 0:
-        print("Stop")
+    if n==0:
+        print("Auguri")
+
+
     #condizione non terminale
     else:
         print(n)
-        # sleep(1)
-        countdown_recursive(n-1)
-
+        sleep(1)
+        countdown_ricorsione(n-1)
 if __name__ == '__main__':
-    N = 4
-    countdown_recursive(N)
+    n=10
+    countdown_ricorsione(n)
